1dfd6a2934c5ccc9d481fb338e3ec1a19609eb9a
gilpop8663 <wolfye@naver.com>
2022-07-30 00:55:58 +0900

    Add: file setup

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold text-red-400 underline">Hello world!</h1>
+  );
+};
+
+export default Home;
