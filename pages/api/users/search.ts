35a40110aa56572e22778e24aeeb1872abcf5bf3
gilpop8663 <wolfye@naver.com>
2022-09-21 19:41:58 +0900

    Edit: .env.development git ignore && api/user/search 파일 수정

--- pages/api/users/search.ts
+++ pages/api/users/search.ts
@@ -9,7 +9,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     session: { user: SESSION_USER },
   } = req;
 
-  const user = await client.CoddinkUser.findMany({
+  const user = await client.coddinkUser.findMany({
     where: {
       name: {
         contains: name?.toString(),
