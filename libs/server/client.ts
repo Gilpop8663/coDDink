a031f4b017cd22e5f0e2dacb559fda7bd1b5989a
gilpop8663 <wolfye@naver.com>
2022-09-21 17:06:44 +0900

    Edit : Prisma 관련 수정

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -8,4 +8,8 @@ const client = global.client || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.client = client;
 
+if (!global.client) {
+  global.client = new PrismaClient();
+}
+
 export default client;
