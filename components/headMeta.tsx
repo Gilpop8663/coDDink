d9679a26ddcbdc22e5c2132016c7d530e3ae27a8
gilpop8663 <wolfye@naver.com>
2022-09-26 17:16:03 +0900

    Add: 네이버 사이트 등록

--- components/headMeta.tsx
+++ components/headMeta.tsx
@@ -16,12 +16,12 @@ export default function HeadMeta({
 }: HeadMetaProps) {
   return (
     <Head>
-      <title>{title || "coDDinK"}</title>
+      <title>{title || "코띵크(coDDinK)"}</title>
       <meta
         name="description"
         content={
           description ||
-          "코띵크(CODDINK)는 개발자들이 만든 사이트 등을 전시하고 검색할 수 있는 무료 온라인 웹 포트폴리오 사이트입니다."
+          "코띵크(coDDinK)는 개발자들이 만든 사이드 프로젝트, 사이트 등을 전시하고 검색할 수 있는 무료 온라인 웹 포트폴리오 사이트입니다."
         }
       ></meta>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -33,10 +33,14 @@ export default function HeadMeta({
         property="og:description"
         content={
           description ||
-          "코띵크(CODDINK)는 개발자들이 만든 사이트 등을 전시하고 검색할 수 있는 무료 온라인 웹 포트폴리오 사이트입니다."
+          "코띵크(coDDinK)는 개발자들이 만든 사이트 등을 전시하고 검색할 수 있는 무료 온라인 웹 포트폴리오 사이트입니다."
         }
       ></meta>
-      <meta property="og:article:author" content="coDinK" />
+      <meta property="og:article:author" content="coDDinK" />
+      <meta
+        name="naver-site-verification"
+        content="c1b1484ecee30f05d1e938d9ee992d6ada1fe55a"
+      />
     </Head>
   );
 }
