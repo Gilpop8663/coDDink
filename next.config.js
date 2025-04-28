efdd52cc11b0d7c1963be0478dba56372281f1d1
gilpop8663 <wolfye@naver.com>
2025-04-29 07:18:13 +0900

    feat: 이미지 캐시 최소 시간 설정

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = removeImports({
   reactStrictMode: true,
   images: {
     domains: ['d3319kcxpye19t.cloudfront.net'],
+    minimumCacheTTL: 31536000, // 캐시 시간을 1년으로 설정
   },
   experimental: { esmExternals: true },
   env: {
